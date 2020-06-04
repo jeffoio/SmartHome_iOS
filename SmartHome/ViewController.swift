7aeb109cb1b3f4c902600f22dc90073fb08c42d4
Jeff <xorgus1234@gmail.com>
2020-06-04 14:18:44 +0900

    Initial Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by jeff on 2020/06/04.
+//  Copyright © 2020 TakHyun Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
