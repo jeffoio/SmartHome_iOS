56f328f2e8a0e7494623f4acb82007a9591a8d1a
Jeff <xorgus1234@gmail.com>
2020-06-04 17:39:31 +0900

    HomeView

--- SmartHome/HomeViewController.swift
+++ SmartHome/HomeViewController.swift
@@ -9,22 +9,50 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var airSwitch: UISwitch!
+    @IBOutlet weak var roomSwitch: UISwitch!
+    @IBOutlet weak var bedSwitch: UISwitch!
+    @IBOutlet weak var airLowToggleBtn: UIButton!
+    @IBOutlet weak var airHighToggleBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Switch resize
+        airSwitch.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        roomSwitch.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        bedSwitch.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func airLowToggleBtn(_ sender: UIButton) {
+        sender.isSelected.toggle()
     }
-    */
-
+    @IBAction func airHighToggleBtn(_ sender: UIButton) {
+        sender.isSelected.toggle()
+    }
+}
+//View에 Border 추가
+extension CALayer {
+    func addBorder(arrEdge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arrEdge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor
+            self.addSublayer(border)
+        }
+    }
 }
--- SmartHome/ViewController.swift
+++ SmartHome/ViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
