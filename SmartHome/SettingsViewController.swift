c9ae414945f35a20617969f24b1e1ab13bcc584c
Jeff <xorgus1234@gmail.com>
2020-06-04 16:59:44 +0900

    HomeView

---
+++ SmartHome/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  SmartHome
+//
+//  Created by jeff on 2020/06/04.
+//  Copyright © 2020 TakHyun Jung. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
